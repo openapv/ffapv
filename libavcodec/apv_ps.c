1ee546ef44cf65b51b91a4861cfa05acd8ea74ea
dkozinski <dkozinsk@interia.pl>
2023-07-26 12:03:28 +0200

    avformat/apv_demuxer: Added demuxer to handle reading APV video files
    - Provided AVInputFormat struct describing APV input format(ff_apv_demuxer)

--- libavcodec/apv_ps.c
+++ libavcodec/apv_ps.c
@@ -42,8 +42,7 @@
 
 
 // @see APV Technical Note 2.6 Entropy coding (Zig-zag scanning of an 8x8 transform block)
-const uint16_t ScanOrder[APV_BLOCK_D] =
-{
+const uint16_t ScanOrder[APV_BLOCK_D] = {
     0,    1,    8,   16,    9,    2,    3,   10,
     17,   24,   32,   25,   18,   11,    4,    5,
     12,   19,   26,   33,   40,   48,   41,   34,
@@ -55,7 +54,8 @@ const uint16_t ScanOrder[APV_BLOCK_D] =
 };
 
 // @see WD1_APV_spec 7.3.1.3 Tile info syntax
-int ff_apv_tile_info(GetBitContext *gb, APVParamSets *ps) {
+int ff_apv_tile_info(GetBitContext *gb, const APVFrameDataHeader *fdh, APVTileInfo *ti)
+{
     // @see WD1_APV_spec section 6.2 Source, decoded and output frame formats
     int MbWidth = APV_MB_WIDTH;
     int MbHeight = APV_MB_HEIGHT;
@@ -68,54 +68,53 @@ int ff_apv_tile_info(GetBitContext *gb, APVParamSets *ps) {
     int startMb = 0;
     int i = 0;
 
-    ps->tile_info.tile_width_in_mbs_minus1 = get_bits(gb, 28);
-    ps->tile_info.tile_height_in_mbs_minus1 = get_bits(gb, 28);
+    ti->tile_width_in_mbs_minus1 = get_bits(gb, 28);
+    ti->tile_height_in_mbs_minus1 = get_bits(gb, 28);
 
-    FrameWidthInSamplesY = ps->frame_data_header.frame_width_minus1 + 1;
-    FrameHeightInSamplesY = ps->frame_data_header.frame_height_minus1 + 1;
+    FrameWidthInSamplesY = fdh->frame_width_minus1 + 1;
+    FrameHeightInSamplesY = fdh->frame_height_minus1 + 1;
     FrameWidthInMbsY = ceil( FrameWidthInSamplesY / MbWidth );
     FrameHeightInMbsY = ceil( FrameHeightInSamplesY / MbHeight );
 
-    if(!ps->tile_info.ColStarts)
-        free(ps->tile_info.ColStarts);
+    if(!ti->ColStarts)
+        free(ti->ColStarts);
 
     // remember to free it
-    ps->tile_info.ColStarts = malloc(sizeof(int)*FrameWidthInMbsY);
+    ti->ColStarts = malloc(sizeof(int) * FrameWidthInMbsY);
 
     for( i = 0; startMb < FrameWidthInMbsY; i++ ) {
-        ps->tile_info.ColStarts[ i ] = startMb * MbWidth;
-        startMb += ps->tile_info.tile_width_in_mbs_minus1 + 1;
+        ti->ColStarts[ i ] = startMb * MbWidth;
+        startMb += ti->tile_width_in_mbs_minus1 + 1;
     }
 
-    ps->tile_info.ColStarts[ i ] = FrameWidthInMbsY * MbWidth;
-    ps->tile_info.TileCols = i;
+    ti->ColStarts[ i ] = FrameWidthInMbsY * MbWidth;
+    ti->TileCols = i;
 
     startMb = 0;
 
-    if(!ps->tile_info.RowStarts)
-        free(ps->tile_info.RowStarts);
+    if(!ti->RowStarts)
+        free(ti->RowStarts);
 
     // remember to free it
-    ps->tile_info.RowStarts = malloc(sizeof(int)*FrameHeightInMbsY);
+    ti->RowStarts = malloc(sizeof(int) * FrameHeightInMbsY);
 
     for( i = 0; startMb < FrameHeightInMbsY; i++ ) {
-        ps->tile_info.RowStarts[ i ] = startMb * MbHeight;
-        startMb += ps->tile_info.tile_height_in_mbs_minus1 + 1;
+        ti->RowStarts[ i ] = startMb * MbHeight;
+        startMb += ti->tile_height_in_mbs_minus1 + 1;
     }
 
-    ps->tile_info.RowStarts[ i ] = FrameHeightInMbsY * MbHeight;
-    ps->tile_info.TileRows = i;
-    ps->NumTiles = ps->tile_info.TileCols * ps->tile_info.TileRows;
+    ti->RowStarts[ i ] = FrameHeightInMbsY * MbHeight;
+    ti->TileRows = i;
+    ti->NumTiles = ti->TileCols * ti->TileRows;
 
-    if(!ps->tile_info.tile_size_minus1)
-        free(ps->tile_info.tile_size_minus1);
+    if(!ti->tile_size_minus1)
+        free(ti->tile_size_minus1);
 
     // remember to free it
-    ps->tile_info.tile_size_minus1 = malloc(sizeof(uint32_t)*ps->NumTiles);
+    ti->tile_size_minus1 = malloc(sizeof(uint32_t) * ti->NumTiles);
 
-    for( i = 0; i < ps->NumTiles - 1; i++ ) {
-        ps->tile_info.tile_size_minus1[i] = get_bits(gb, 24);
-    }
+    for( i = 0; i < ti->NumTiles - 1; i++ )
+        ti->tile_size_minus1[i] = get_bits(gb, 24);
 
     return 0;
 }
@@ -125,81 +124,82 @@ int ff_apv_tile_info(GetBitContext *gb, APVParamSets *ps) {
 // — If the current position in the bitstream is on a byte boundary, i.e., the next bit in the bitstream is the first bit in a byte, the return value of byte_aligned( ) is equal to TRUE.
 // — Otherwise, the return value of byte_aligned( ) is equal to FALSE.
 //
-static int byte_aligned(GetBitContext *gb) {
+static int byte_aligned(GetBitContext *gb)
+{
     int bits_count = get_bits_count(gb);
-    if (bits_count % 8) {
+    if (bits_count % 8)
         return 1;
-    } else {
+    else
         return 0;
-    }
 }
 
 // @see WD1_APV_spec 7.3.3
-int ff_apv_byte_alignment(GetBitContext *gb, APVParamSets *ps) {
-    while(!byte_aligned(gb)) {
-        ps->alignment_bit_equal_to_zero = get_bits1(gb);
-    }
+int ff_apv_byte_alignment(GetBitContext *gb, APVByteAlignemnt *ba)
+{
+    while(!byte_aligned(gb))
+        ba->alignment_bit_equal_to_zero = get_bits1(gb);
     return 0;
 }
 
 // @see WD1_APV_spec 7.3.1.2 Quantization matrix syntax
-int ff_apv_quantization_matrix(GetBitContext *gb, APVParamSets *ps) {
+int ff_apv_quantization_matrix(GetBitContext *gb, APVQuantizationMatrix *qm)
+{
     for( int cIdx = 0; cIdx < 3; cIdx ++ ) {
         for( int y = 0; y < 8; y ++ ) {
-            for( int x = 0; x < 8; x ++ ) {
-                ps->q_matrix_minus1[ cIdx ][ x ][ y ] = get_bits(gb, 8);
-            }
+            for( int x = 0; x < 8; x ++ )
+                qm->q_matrix_minus1[ cIdx ][ x ][ y ] = get_bits(gb, 8);
         }
     }
     return 0;
 }
 
 // @see WD1_APV_spec 7.3.1.1 Frame header syntax
-int ff_apv_parse_frame_header(GetBitContext *gb, APVParamSets *ps) {
+int ff_apv_parse_frame_header(GetBitContext *gb, APVFrameDataHeader *fdh)
+{
 
     int ret = 0;
     // @todo parese frame header
-    ps->frame_data_header.frame_header_size = get_bits(gb, 16);
-    ps->frame_data_header.profile_idc = get_bits(gb, 8);
-    ps->frame_data_header.level_idc = get_bits(gb, 8);
-    ps->frame_data_header.reserved_zero_8bits = get_bits(gb, 8);
-    ps->frame_data_header.frame_width_minus1 = get_bits(gb, 32);
-    ps->frame_data_header.frame_height_minus1 = get_bits(gb, 32);
-    ps->frame_data_header.chroma_format_idc = get_bits(gb, 2);
-    ps->frame_data_header.bit_depth_minus8 = get_bits(gb, 4);
-    ps->frame_data_header.capture_time_distance = get_bits(gb, 8);
-    ps->frame_data_header.reserved_zero_16bits = get_bits(gb, 16);
-    ps->frame_data_header.color_description_present_flag = get_bits(gb, 1);
+    fdh->frame_header_size = get_bits(gb, 16);
+    fdh->profile_idc = get_bits(gb, 8);
+    fdh->level_idc = get_bits(gb, 8);
+    fdh->reserved_zero_8bits = get_bits(gb, 8);
+    fdh->frame_width_minus1 = get_bits(gb, 32);
+    fdh->frame_height_minus1 = get_bits(gb, 32);
+    fdh->chroma_format_idc = get_bits(gb, 2);
+    fdh->bit_depth_minus8 = get_bits(gb, 4);
+    fdh->capture_time_distance = get_bits(gb, 8);
+    fdh->reserved_zero_16bits = get_bits(gb, 16);
+    fdh->color_description_present_flag = get_bits(gb, 1);
 
-    if(ps->frame_data_header.color_description_present_flag) {
-        ps->frame_data_header.color_primaries = get_bits(gb, 8);
-        ps->frame_data_header.transfer_characteristics = get_bits(gb, 8);
-        ps->frame_data_header.matrix_coefficients = get_bits(gb, 8);
+    if(fdh->color_description_present_flag) {
+        fdh->color_primaries = get_bits(gb, 8);
+        fdh->transfer_characteristics = get_bits(gb, 8);
+        fdh->matrix_coefficients = get_bits(gb, 8);
     }
-    ps->frame_data_header.use_q_matrix = get_bits(gb, 1);
-    if(ps->frame_data_header.use_q_matrix) {
-        ret = ff_apv_quantization_matrix(gb, ps);
-    }
+    fdh->use_q_matrix = get_bits(gb, 1);
+    if(fdh->use_q_matrix)
+        ret = ff_apv_quantization_matrix(gb, &fdh->quantization_matrix);
 
-    ff_apv_tile_info(gb, ps);
-    ps->frame_data_header.reserved_zero_8bits_2 = get_bits(gb, 8);
+    ff_apv_tile_info(gb, fdh, &fdh->tile_info);
+    fdh->reserved_zero_8bits_2 = get_bits(gb, 8);
 
-    ff_apv_byte_alignment(gb, ps);
+    ff_apv_byte_alignment(gb, &fdh->byte_alignent);
 
     return ret;
 }
 
 // @see WD1_APV_spec 9.1.4 Parsing process for variable length codes
-static int read_vlc(GetBitContext *gb, uint32_t kParam) {
+static int read_vlc(GetBitContext *gb, uint32_t kParam)
+{
 
     uint32_t symbolValue = 0;
     uint8_t parseExpGolomb = 1;
     uint32_t k = kParam;
     uint32_t stopLoop = 0;
 
-    if( get_bits(gb, 1) == 1 ) {
+    if( get_bits(gb, 1) == 1 )
         parseExpGolomb = 0;
-    } else {
+    else {
         if( get_bits(gb, 1) == 0 ) {
             symbolValue += ( 1 << k );
             parseExpGolomb = 0;
@@ -211,9 +211,9 @@ static int read_vlc(GetBitContext *gb, uint32_t kParam) {
 
     if( parseExpGolomb ) {
         do {
-            if( get_bits(gb, 1) == 1 ) {
+            if( get_bits(gb, 1) == 1 )
                 stopLoop = 1;
-            } else {
+            else {
                 symbolValue += ( 1 << k );
                 k++;
             }
@@ -227,7 +227,8 @@ static int read_vlc(GetBitContext *gb, uint32_t kParam) {
 }
 
 // @see WD1_APV_spec 7.3.2.4 AC coefficient codining syntax
-static void ac_coeff_coding( GetBitContext *gb, uint32_t Prev1stAcLevel, int32_t TransCoeff[APV_COLOR_COMP_NUM][APV_MB_WIDTH][APV_MB_HEIGHT], uint32_t x0, uint32_t y0, int log2BlkWidth, int log2BlkHeight, int cIdx ) {
+static void ac_coeff_coding( GetBitContext *gb, uint32_t Prev1stAcLevel, int32_t TransCoeff[APV_COLOR_COMP_NUM][APV_MB_WIDTH][APV_MB_HEIGHT], uint32_t x0, uint32_t y0, int log2BlkWidth, int log2BlkHeight, int cIdx )
+{
     int scanPos = 1;
     int firstAC = 1;
     int PrevLevel = Prev1stAcLevel;
@@ -245,7 +246,7 @@ static void ac_coeff_coding( GetBitContext *gb, uint32_t Prev1stAcLevel, int32_t
             scanPos++;
         }
         PrevRun = coeff_zero_run;
-        if( scanPos < ( 1<< ( log2BlkWidth + log2BlkHeight ) ) ) {
+        if( scanPos < ( 1 << ( log2BlkWidth + log2BlkHeight ) ) ) {
             int kParam = ffapv_clip3( 0, 4, PrevLevel >> 2 );
             int abs_ac_coeff_minus1 = read_vlc(gb, kParam);
 
@@ -262,18 +263,19 @@ static void ac_coeff_coding( GetBitContext *gb, uint32_t Prev1stAcLevel, int32_t
                 Prev1stAcLevel = PrevLevel;
             }
         }
-    } while ( scanPos < ( 1<< ( log2BlkWidth + log2BlkHeight ) ) );
+    } while ( scanPos < ( 1 << ( log2BlkWidth + log2BlkHeight ) ) );
 }
 
 // @see WD1_APV_spec 7.3.2.3 Macroblock layer syntax
-static int macroblock_layer( GetBitContext *gb, APVParamSets *ps, uint32_t Prev1stAcLevel,  uint32_t xMb, uint32_t yMb, uint32_t cIdx ) {
+static int macroblock_layer( GetBitContext *gb, const APVFrameDataHeader *fdh, uint32_t Prev1stAcLevel,  uint32_t xMb, uint32_t yMb, uint32_t cIdx )
+{
 
     uint32_t MbWidth = APV_MB_WIDTH;
     uint32_t MbHeight = APV_MB_HEIGHT;
 
     // @see 6.2 Source, decoded and output frame formats
     // @see Table 2 — SubWidthC and SubHeightC values derived from chroma_format_idc
-    uint32_t SubWidthC = (ps->frame_data_header.chroma_format_idc==2)? 2: 1;
+    uint32_t SubWidthC = (fdh->chroma_format_idc == 2) ? 2 : 1;
     uint32_t SubHeightC = 1;
 
     // @see 6.2 Source, decoded and output frame formats
@@ -313,34 +315,36 @@ static int macroblock_layer( GetBitContext *gb, APVParamSets *ps, uint32_t Prev1
 }
 
 // @see WD1_APV_spec 7.3.2.1 Tile header synatx
-static int tile_header(GetBitContext *gb, APVParamSets *ps) {
-    ps->tile_header.tile_header_size = get_bits(gb, 16);
-    ps->tile_header.tile_qp = get_bits(gb, 7);
-    ps->tile_header.tile_cb_qp = get_bits(gb, 7);
-    ps->tile_header.tile_cr_qp = get_bits(gb, 7);
-    ps->tile_header.tile_data_size_y_minus1 = get_bits(gb, 24);
-    ps->tile_header.tile_data_size_cb_minus1 = get_bits(gb, 24);
-    ps->tile_header.tile_data_size_cr_minus1 = get_bits(gb, 24);
+static int tile_header(GetBitContext *gb, APVTileHeader *th)
+{
+    th->tile_header_size = get_bits(gb, 16);
+    th->tile_qp = get_bits(gb, 7);
+    th->tile_cb_qp = get_bits(gb, 7);
+    th->tile_cr_qp = get_bits(gb, 7);
+    th->tile_data_size_y_minus1 = get_bits(gb, 24);
+    th->tile_data_size_cb_minus1 = get_bits(gb, 24);
+    th->tile_data_size_cr_minus1 = get_bits(gb, 24);
 
-    ff_apv_byte_alignment(gb, ps);
+    ff_apv_byte_alignment(gb, &th->byte_alignent);
 
     return 0;
 }
 
 // @see WD1_APV_spec 7.3.2.2 Tile data synatx
 // @param cIdx specifying the color component of the current block,
-static int tile_data(GetBitContext *gb, APVParamSets *ps, uint8_t tileIdx, uint8_t cIdx) {
+static int tile_data(GetBitContext *gb, const APVFrameDataHeader *fdh, APVTile *tile, uint8_t tileIdx, uint8_t cIdx)
+{
     int MbWidth = APV_MB_WIDTH;
     int MbHeight = APV_MB_HEIGHT;
 
-    uint32_t x0 = ps->tile_info.ColStarts[ tileIdx % ps->tile_info.TileCols ];
-    uint32_t y0 = ps->tile_info.RowStarts[ tileIdx / ps->tile_info.TileCols ];
+    uint32_t x0 = fdh->tile_info.ColStarts[ tileIdx % fdh->tile_info.TileCols ];
+    uint32_t y0 = fdh->tile_info.RowStarts[ tileIdx / fdh->tile_info.TileCols ];
 
-    uint32_t numMbColsInTile = ( ps->tile_info.ColStarts[ tileIdx % ps->tile_info.TileCols + 1 ]
-                               - ps->tile_info.ColStarts[ tileIdx % ps->tile_info.TileCols ] ) / MbWidth;
+    uint32_t numMbColsInTile = ( fdh->tile_info.ColStarts[ tileIdx % fdh->tile_info.TileCols + 1 ]
+                                 - fdh->tile_info.ColStarts[ tileIdx % fdh->tile_info.TileCols ] ) / MbWidth;
 
-    uint32_t numMbRowsInTile = ( ps->tile_info.RowStarts[ tileIdx / ps->tile_info.TileCols + 1 ]
-                               - ps->tile_info.RowStarts[ tileIdx / ps->tile_info.TileCols] ) / MbHeight;
+    uint32_t numMbRowsInTile = ( fdh->tile_info.RowStarts[ tileIdx / fdh->tile_info.TileCols + 1 ]
+                                 - fdh->tile_info.RowStarts[ tileIdx / fdh->tile_info.TileCols] ) / MbHeight;
 
     uint32_t numMbsInTile = numMbColsInTile * numMbRowsInTile;
 
@@ -348,38 +352,47 @@ static int tile_data(GetBitContext *gb, APVParamSets *ps, uint8_t tileIdx, uint8
     for( int i = 0; i < numMbsInTile; i++ ) {
         uint32_t xMb = x0 + ( i % numMbColsInTile ) * MbWidth;
         uint32_t yMb = y0 + ( i / numMbColsInTile ) * MbHeight;
-        macroblock_layer( gb, ps, Prev1stAcLevel, xMb, yMb, cIdx );
+        macroblock_layer( gb, fdh, Prev1stAcLevel, xMb, yMb, cIdx );
     }
-    ff_apv_byte_alignment(gb, ps);
+    ff_apv_byte_alignment(gb, &tile->tile_data->byte_alignent);
 
     return 0;
 }
 
 // @see WD1_APV_spec 7.3.2 Tile synatx
-int ff_apv_parse_tile(GetBitContext *gb, APVParamSets *ps, uint8_t tileIdx) {
+int ff_apv_parse_tile(GetBitContext *gb, const APVFrameDataHeader *fdh, APVTile *tile, uint8_t tileIdx)
+{
     int ret = 0;
-    tile_header(gb, ps);
-    tile_data(gb, ps, tileIdx, 0);
-    tile_data(gb, ps, tileIdx, 1);
-    tile_data(gb, ps, tileIdx, 2);
+    tile_header(gb, &tile->tile_header);
+    tile_data(gb, fdh, tile, tileIdx, 0);
+    tile_data(gb, fdh, tile, tileIdx, 1);
+    tile_data(gb, fdh, tile, tileIdx, 2);
 
     return ret;
 }
 
-void ff_apv_ps_free(APVParamSets *ps) {
+void ff_apv_ps_free(APVParamSets *ps)
+{
+    APVFrameData fd = ps->frame_data;
+
+    for( int i = 0; i < fd.NumTiles; i++ )
+        free(fd.tiles[i]);
+
+    free(fd.tiles);
 }
 
 // @see WD1_APV_spec Annex C C.1.1 Metadata synatx
 // @todo provide implementation
-static int metadata_payload( GetBitContext *gb, uint32_t metadataSize ) {
+static int metadata_payload( GetBitContext *gb, uint32_t metadataSize )
+{
     do {
         uint16_t size;
 
         skip_bits(gb, 16);          // u(16)
 
         size = get_bits(gb, 16);    // u(16)
-        
-        skip_bits(gb, size*8);      // value u(size*8);
+
+        skip_bits(gb, size * 8);    // value u(size*8);
 
     } while (get_bits(gb, 16) != 0xFFFF);
 
@@ -387,17 +400,19 @@ static int metadata_payload( GetBitContext *gb, uint32_t metadataSize ) {
 }
 
 // @see WD1_APV_spec 7.3.1.4 Metadata syntax
-int ff_apv_parse_metadata(GetBitContext *gb, APVParamSets *ps) {
+int ff_apv_parse_metadata(GetBitContext *gb, APVFrameData *fd)
+{
     uint16_t metadata_num = get_bits(gb, 16); // u(16)
     for(int i = 0; i < metadata_num; i++ ) {
-        uint32_t metadata_size_minus1 = get_bits(gb, 32); // u(32)
+        uint32_t metadata_size_minus1 = get_bits_long(gb, 32); // u(32)
         metadata_payload( gb, metadata_size_minus1 + 1 );
     }
     return 0;
 }
 
 // @see WD1_APV_spec 7.3.1.5 Filler data syntax
-int ff_apv_parse_filler_data(GetBitContext *gb, APVParamSets *ps) {
+int ff_apv_parse_filler_data(GetBitContext *gb, APVFrameData *fd)
+{
     uint8_t ff_byte = 0;
     while(  get_bits(gb, 8) == 0xFF )
         ff_byte = 0xff; // equal to 0xFF
