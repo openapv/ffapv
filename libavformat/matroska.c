1dd96cdb2e0a3778aba3e765bb87ffa8047f4f8b
Dawid Kozinski <d.kozinski@samsung.com>
2024-03-13 10:17:11 +0100

    libavformat/matroska: Added tag for APV codec

--- libavformat/matroska.c
+++ libavformat/matroska.c
@@ -77,6 +77,7 @@ const CodecTags ff_mkv_codec_tags[]={
     {"S_HDMV/TEXTST"    , AV_CODEC_ID_HDMV_TEXT_SUBTITLE},
     {"S_ARIBSUB"        , AV_CODEC_ID_ARIB_CAPTION},
 
+    {"V_APV"            , AV_CODEC_ID_APV},
     {"V_AV1"            , AV_CODEC_ID_AV1},
     {"V_AVS2"           , AV_CODEC_ID_AVS2},
     {"V_AVS3"           , AV_CODEC_ID_AVS3},
