c4a1597366797d06c8d27fda8a18217d05048d3e
Dawid Kozinski <2955139+dkozinski@users.noreply.github.com>
2024-09-25 13:03:55 +0200

    avcodec/apv_encoder: Set flags needed to build the index necessary for seeking in the APV video stream put inside the NUT container

--- libavcodec/libapvenc.c
+++ libavcodec/libapvenc.c
@@ -394,8 +394,10 @@ static int libapve_encode(AVCodecContext *avctx, AVPacket *avpkt,
 
         avpkt->time_base.num = 1;
         avpkt->time_base.den = apvctx->cdsc.param.fps;
-        
-        avpkt->pts = avpkt->dts = frame->pts + 1;  // @todo provide implementation in APV apvctx->bitb.ts[0];
+
+        avpkt->pts = avpkt->dts = frame->pts;  // @todo provide implementation in APV apvctx->bitb.ts[0];
+        avpkt->flags |= AV_PKT_FLAG_KEY;
+
         ff_side_data_set_encoder_stats(avpkt, apvctx->stat.qp * FF_QP2LAMBDA, NULL, 0, AV_PICTURE_TYPE_I);
 
         *got_packet = 1;
