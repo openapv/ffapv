11ad4f6f40d19e3eed2656a842d2eda8e64e7a41
Dawid Kozinski <d.kozinski@samsung.com>
2025-02-14 14:27:28 +0100

    avcodec/apv_decoder: Changed name of the APV encoder implementation
    - Renamed the APV encoder from libapve to liboapv for consistency and clarity in naming
    - Updated the properties of the APNG codec descriptor to include AV_CODEC_PROP_INTRA_ONLY, indicating that it supports only intraframe coding
    
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

--- libavcodec/codec_desc.c
+++ libavcodec/codec_desc.c
@@ -1541,7 +1541,7 @@ static const AVCodecDescriptor codec_descriptors[] = {
         .type      = AVMEDIA_TYPE_VIDEO,
         .name      = "apng",
         .long_name = NULL_IF_CONFIG_SMALL("APNG (Animated Portable Network Graphics) image"),
-        .props     = AV_CODEC_PROP_LOSSLESS,
+        .props     = AV_CODEC_PROP_LOSSLESS | AV_CODEC_PROP_INTRA_ONLY,
         .mime_types= MT("image/png"),
     },
     {
