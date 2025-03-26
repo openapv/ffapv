0f4536ba38998c5a888545b9f4b9f5ab15a14aad
Dawid Kozinski <d.kozinski@samsung.com>
2025-03-26 09:35:07 +0100

    avformat/movmuxer: Updated apvC to be defined as a FullBox instead of a Box to comply the specification
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

--- libavformat/movenc.c
+++ libavformat/movenc.c
@@ -1651,6 +1651,9 @@ static int mov_write_apvc_tag(AVIOContext *pb, MOVTrack *track)
     avio_wb32(pb, 0);
     ffio_wfourcc(pb, "apvC");
 
+    avio_w8  (pb, 0); /* version */
+    avio_wb24(pb, 0); /* flags */
+    
     if (track->tag == MKTAG('a','p','v','1'))
         ff_isom_write_apvc(pb, track->vos_data, track->vos_len, 1);
     else
