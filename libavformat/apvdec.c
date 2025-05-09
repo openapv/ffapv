75a449a5596bc643aec2470474e705ce1547b3f4
Dawid Kozinski <d.kozinski@samsung.com>
2025-05-09 12:52:09 +0200

    avformat/apv_demuxer: Removed reading codec level information
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

--- libavformat/apvdec.c
+++ libavformat/apvdec.c
@@ -36,24 +36,13 @@ typedef struct APVHeaderInfo {
     int      frame_width;
     int      frame_height;
 
-    uint8_t  chroma_format_idc;
     uint8_t  bit_depth_minus8;
-
-    enum AVPixelFormat pixel_format;
 } APVHeaderInfo;
 
-static const enum AVPixelFormat apv_format_table[5][5] = {
-    { AV_PIX_FMT_GRAY8,    AV_PIX_FMT_GRAY10,     AV_PIX_FMT_GRAY12,     AV_PIX_FMT_GRAY14, AV_PIX_FMT_GRAY16 },
-    { 0 }, // 4:2:0 is not valid.
-    { AV_PIX_FMT_YUV422P,  AV_PIX_FMT_YUV422P10,  AV_PIX_FMT_YUV422P12,  AV_PIX_FMT_GRAY14, AV_PIX_FMT_YUV422P16 },
-    { AV_PIX_FMT_YUV444P,  AV_PIX_FMT_YUV444P10,  AV_PIX_FMT_YUV444P12,  AV_PIX_FMT_GRAY14, AV_PIX_FMT_YUV444P16 },
-    { AV_PIX_FMT_YUVA444P, AV_PIX_FMT_YUVA444P10, AV_PIX_FMT_YUVA444P12, AV_PIX_FMT_GRAY14, AV_PIX_FMT_YUVA444P16 },
-};
-
-static int apv_extract_header_info(APVHeaderInfo *info,
-                                   GetByteContext *gbc)
+static int apv_extract_header_info(GetByteContext *gbc)
 {
-    int zero, byte, bit_depth_index;
+    APVHeaderInfo header, *info = &header;
+    int zero, byte;
 
     info->pbu_type = bytestream2_get_byte(gbc);
     info->group_id = bytestream2_get_be16(gbc);
@@ -62,7 +51,24 @@ static int apv_extract_header_info(APVHeaderInfo *info,
     if (zero != 0)
         return AVERROR_INVALIDDATA;
 
-    if (info->pbu_type != APV_PBU_PRIMARY_FRAME)
+    if (info->pbu_type == APV_PBU_ACCESS_UNIT_INFORMATION) {
+        unsigned int num_frames = bytestream2_get_be16(gbc);
+        int pbu_type;
+
+        if (num_frames < 1)
+            return AVERROR_INVALIDDATA;
+
+        info->pbu_type = bytestream2_get_byte(gbc);
+        if (info->pbu_type != APV_PBU_PRIMARY_FRAME &&
+            info->pbu_type != APV_PBU_NON_PRIMARY_FRAME &&
+            (info->pbu_type < APV_PBU_PREVIEW_FRAME || info->pbu_type > APV_PBU_ALPHA_FRAME))
+            return AVERROR_INVALIDDATA;
+
+        bytestream2_skip(gbc, 2); // group_id
+        zero = bytestream2_get_byte(gbc);
+        if (zero != 0)
+            return AVERROR_INVALIDDATA;
+    } else if (info->pbu_type != APV_PBU_PRIMARY_FRAME)
         return AVERROR_INVALIDDATA;
 
     info->profile_idc = bytestream2_get_byte(gbc);
@@ -81,7 +87,6 @@ static int apv_extract_header_info(APVHeaderInfo *info,
         return AVERROR_INVALIDDATA;
 
     byte = bytestream2_get_byte(gbc);
-    info->chroma_format_idc = byte >> 4;
     info->bit_depth_minus8  = byte & 0xf;
 
     if (info->bit_depth_minus8 > 8) {
@@ -91,19 +96,7 @@ static int apv_extract_header_info(APVHeaderInfo *info,
         // Odd bit depths are technically valid but not useful here.
         return AVERROR_INVALIDDATA;
     }
-    bit_depth_index = info->bit_depth_minus8 / 2;
 
-    switch (info->chroma_format_idc) {
-    case APV_CHROMA_FORMAT_400:
-    case APV_CHROMA_FORMAT_422:
-    case APV_CHROMA_FORMAT_444:
-    case APV_CHROMA_FORMAT_4444:
-        info->pixel_format = apv_format_table[info->chroma_format_idc][bit_depth_index];
-        break;
-    default:
-        return AVERROR_INVALIDDATA;
-    }
-    
     // Ignore capture_time_distance.
     bytestream2_skip(gbc, 1);
 
@@ -117,7 +110,6 @@ static int apv_extract_header_info(APVHeaderInfo *info,
 static int apv_probe(const AVProbeData *p)
 {
     GetByteContext gbc;
-    APVHeaderInfo header;
     uint32_t au_size, signature, pbu_size;
     int err;
 
@@ -144,7 +136,7 @@ static int apv_probe(const AVProbeData *p)
         return 0;
     }
 
-    err = apv_extract_header_info(&header, &gbc);
+    err = apv_extract_header_info(&gbc);
     if (err < 0) {
         // Header does not look like APV.
         return 0;
@@ -156,15 +148,14 @@ static int apv_read_header(AVFormatContext *s)
 {
     AVStream *st;
     GetByteContext gbc;
-    APVHeaderInfo header;
-    uint8_t buffer[28];
+    uint8_t buffer[12];
     uint32_t au_size, signature, pbu_size;
     int err, size;
 
     err = ffio_ensure_seekback(s->pb, sizeof(buffer));
     if (err < 0)
         return err;
-    size = avio_read(s->pb, buffer, sizeof(buffer));
+    size = ffio_read_size(s->pb, buffer, sizeof(buffer));
     if (size < 0)
         return size;
 
@@ -186,22 +177,14 @@ static int apv_read_header(AVFormatContext *s)
         return AVERROR_INVALIDDATA;
     }
 
-    err = apv_extract_header_info(&header, &gbc);
-    if (err < 0)
-        return err;
-
     st = avformat_new_stream(s, NULL);
     if (!st)
         return AVERROR(ENOMEM);
 
     st->codecpar->codec_type = AVMEDIA_TYPE_VIDEO;
     st->codecpar->codec_id   = AV_CODEC_ID_APV;
-    st->codecpar->format     = header.pixel_format;
-    st->codecpar->profile    = header.profile_idc;
-    st->codecpar->level      = header.level_idc;
-    st->codecpar->width      = header.frame_width;
-    st->codecpar->height     = header.frame_height;
 
+    ffstream(st)->need_parsing = AVSTREAM_PARSE_HEADERS;
     st->avg_frame_rate = (AVRational){ 30, 1 };
     avpriv_set_pts_info(st, 64, 1, 30);
 
@@ -214,7 +197,7 @@ static int apv_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
     uint32_t au_size, signature;
     int ret;
-     
+
     au_size = avio_rb32(s->pb);
     if (au_size == 0 && avio_feof(s->pb))
         return AVERROR_EOF;
@@ -223,16 +206,20 @@ static int apv_read_packet(AVFormatContext *s, AVPacket *pkt)
                "APV AU has invalid size: %"PRIu32"\n", au_size);
         return AVERROR_INVALIDDATA;
     }
-     
+
     ret = av_get_packet(s->pb, pkt, au_size);
-    pkt->flags        = AV_PKT_FLAG_KEY;
-    
+    if (ret < 0)
+        return ret;
+    pkt->pos  -= 4;
+    pkt->flags = AV_PKT_FLAG_KEY;
+
     signature = AV_RB32(pkt->data);
     if (signature != APV_SIGNATURE) {
         av_log(s, AV_LOG_ERROR, "APV AU has invalid signature.\n");
         return AVERROR_INVALIDDATA;
     }
-    return ret;
+
+    return 0;
 }
 
 const FFInputFormat ff_apv_demuxer = {
@@ -240,7 +227,6 @@ const FFInputFormat ff_apv_demuxer = {
     .p.long_name    = NULL_IF_CONFIG_SMALL("APV raw bitstream"),
     .p.extensions   = "apv",
     .p.flags        = AVFMT_GENERIC_INDEX | AVFMT_NOTIMESTAMPS,
-    .flags_internal = FF_INFMT_FLAG_INIT_CLEANUP,
     .read_probe     = apv_probe,
     .read_header    = apv_read_header,
     .read_packet    = apv_read_packet,
