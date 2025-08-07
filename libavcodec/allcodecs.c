332a60cf92b08c6a451568ae27c3fe8f89cee07b
Dawid Kozinski <d.kozinski@samsung.com>
2025-08-07 10:07:43 +0200

    avcodec/oapv_decoder: Provided support for libopenapv APV decoder
    - Added APV decoder wrapper
    - Changes in project configuration file and libavcodec Makefile
    - Added documentation for APV decoder wrapper
    
    Signed-off-by: Dawid Kozinski <d.kozinski@samsung.com>

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -790,6 +790,7 @@ extern const FFCodec ff_liblc3_encoder;
 extern const FFCodec ff_liblc3_decoder;
 extern const FFCodec ff_libmp3lame_encoder;
 extern const FFCodec ff_liboapv_encoder;
+extern const FFCodec ff_liboapv_decoder;
 extern const FFCodec ff_libopencore_amrnb_encoder;
 extern const FFCodec ff_libopencore_amrnb_decoder;
 extern const FFCodec ff_libopencore_amrwb_decoder;
